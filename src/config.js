79e39a9f6ea4b666dd064ee64edab9d0a9428888
Muhammad Abdul Rafey Farooqi <abdul.rafey@sarmaaya.pk>
2025-12-25 12:39:42 +0500

    urdu name added

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const SITE_TITLE = "Muhammad A. Rafey Farooqi";
+export const SITE_TITLE_URDU = "محمد عبدالرافع فاروقی";
 export const SITE_DESCRIPTION = "mfarooqi [dot] bese21seecs [at] seecs [dot] edu [dot] pk";
